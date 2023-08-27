ad13e15ee2a3e75dfa3df7505ad6e0d37a34cdd8
MityA32 <hetmandmitriy@gmail.com>
2023-08-27 05:36:22 +0300

    extensions and costs

---
+++ WarDataMonitor/Utils/Const/Const.swift
@@ -0,0 +1,16 @@
+//
+//  Const.swift
+//  WarDataMonitor
+//
+//  Created by Dmytro Hetman on 27.08.2023.
+//
+
+import Foundation
+
+enum WarDataRequest {
+    static let username = "MacPaw"
+    static let repository = "2022-Ukraine-Russia-War-Dataset"
+    static let branch = "main"
+    static let personnelFilePath = "data/russia_losses_personnel.json"
+    static let equipmentFilePath = "data/russia_losses_equipment.json"
+}
